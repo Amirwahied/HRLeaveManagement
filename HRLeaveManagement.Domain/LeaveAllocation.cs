21cbb9aa8f87651651e8b53c2805878a2d301b98
AmirWahied <amerwaheid@gmail.com>
2024-02-12 04:33:51 +0200

    Initial Commit

---
+++ HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Domain.Common;
+using HRLeaveManagement.Domain.Common;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveAllocation : BaseEntity
+{
+    public LeaveType? LeaveType { get; set; }
+
+    public int NumberOfDays { get; set; }
+
+    public int LeaveTypeId { get; set; }
+
+    public int Period { get; set; }
+}
---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using HRLeaveManagement.Domain.Common;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public required string Name { get; set; }
+    public int DefaultDays { get; set; }
+}
